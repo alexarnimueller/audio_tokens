0a221806d62c2b0df62ef7491ec035df802bec63
Alex Müller <alexarnimueller@protonmail.com>
2024-03-17 15:53:06 +0100

    :busts_in_silhouette: clustering :arrows_counterclockwise:

--- js/jspsych-audio-tokens-v7.js
+++ js/jspsych-audio-tokens-v7.js
@@ -45,7 +45,7 @@ var jsPsychAudioTokens = (function (jspsych) {
         type: jspsych.ParameterType.HTML_STRING,
         pretty_name: "Prompt",
         default: null,
-      },      
+      },
       mute_key: {
         type: jspsych.ParameterType.KEYS,
         default: ''
@@ -73,22 +73,24 @@ var jsPsychAudioTokens = (function (jspsych) {
       } else {
         trial.opacity = 1.
       }
-      trial.width = 400
+      trial.width = 600
       trial.trial_id = ''
       trial.nextURL = display_element
-      if (trial.ratingtype=='categories') {
+      if (trial.ratingtype == 'categories') {
         trial.item_spacing = 12.5
       } else {
         trial.item_spacing = 7.5
       }
       trial.jsPsych = this.jsPsych
 
-      var height = {'features': 150*trial.label.length,
-                    'cluster': 400,
-                    'similarity': 400,
-                    'features2d': 375,
-                    'categories': 225,
-                    'triplets': 150}
+      var height = {
+        'features': 150 * trial.label.length,
+        'cluster': 600,
+        'similarity': 400,
+        'features2d': 375,
+        'categories': 225,
+        'triplets': 150
+      }
 
       // Generate html string
       var html = ''
@@ -101,7 +103,7 @@ var jsPsychAudioTokens = (function (jspsych) {
       html += '<div class="container" style="margin-bottom:25px">'
       html += '<div class="d-flex justify-content-center">'
       html += '<div id="plot-speakers-div">'
-      html += '<svg id="plot-speakers" width="400" height="'+height[trial.ratingtype]+'">'
+      html += '<svg id="plot-speakers" width="600" height="' + height[trial.ratingtype] + '">'
       html += '</div>'
       html += '</div>'
       html += '<div id="audio-container"></div>'
@@ -116,37 +118,37 @@ var jsPsychAudioTokens = (function (jspsych) {
       // Generate data for graph
       var i
       var num_speakers = trial.stimuli.length
-      var data = {'nodes': []}
-      for (i=0; i<num_speakers; i++) {
+      var data = { 'nodes': [] }
+      for (i = 0; i < num_speakers; i++) {
         data.nodes.push({
-          'id': 'item-'+String(i), 
+          'id': 'item-' + String(i),
           'audiofile': trial.stimuli[i],
           'x': [], 'y': []
         })
       }
 
       // Generate graph
-      if (trial.ratingtype=='cluster') {
+      if (trial.ratingtype == 'cluster') {
         var graph = new CircleSortGraph(
           data, 'plot-speakers', 'audio-container', 'button-container', true, trial
         )
-      } else if (trial.ratingtype=='similarity') {
+      } else if (trial.ratingtype == 'similarity') {
         var graph = new AudioGraph(
           data, 'plot-speakers', 'audio-container', 'button-container', true, trial
         )
-      } else if (trial.ratingtype=='features') {
+      } else if (trial.ratingtype == 'features') {
         var graph = new FeatureRatings(
           data, 'plot-speakers', 'audio-container', 'button-container', true, trial
         )
-      } else if (trial.ratingtype=='features2d') {
+      } else if (trial.ratingtype == 'features2d') {
         var graph = new FeatureRatings2D(
           data, 'plot-speakers', 'audio-container', 'button-container', true, trial
         )
-      } else if (trial.ratingtype=='categories') {
+      } else if (trial.ratingtype == 'categories') {
         var graph = new FreesortGraph(
           data, 'plot-speakers', 'audio-container', 'button-container', true, trial
         )
-      } else if (trial.ratingtype=='triplets') {
+      } else if (trial.ratingtype == 'triplets') {
         var graph = new TripletAudioGraph(
           data, 'plot-speakers', 'audio-container', 'button-container', true, trial
         )
